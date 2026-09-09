5ca62f3aff4c97ca348ea3c15df54b4490f5c27e
DGNA <NoriegaADG@outlook.com>
2026-09-09 14:38:39 -0600

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/mx/noriegaadg/geopredios_backend/GeoprediosBackendApplication.java
@@ -0,0 +1,13 @@
+package mx.noriegaadg.geopredios_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoprediosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoprediosBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mx/NoriegaADG/geopredios_backend/GeoprediosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.NoriegaADG.geopredios_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoprediosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
